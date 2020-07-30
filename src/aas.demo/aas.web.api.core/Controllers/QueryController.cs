e0df0a6b42e5dad2fb4d4d258c7ba95a981456a0
Bojan Vrhovnik <bojan@vrhovnik.net>
2020-07-30 10:59:42 +0200

    default empty project .NET core checkin

---
+++ src/aas.demo/aas.web.api.core/Controllers/QueryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aas.web.api.Controllers
+{
+    [ApiController]
+    [Route("query")]
+    public class QueryController : ControllerBase
+    {
+        private readonly ILogger<QueryController> logger;
+
+        public QueryController(ILogger<QueryController> logger) => this.logger = logger;
+
+        [HttpGet]
+        public string Get()
+        {
+            logger.LogInformation("Calling get data");
+            var rng = new Random();
+            return rng.ToString();
+        }
+    }
+}
